67321778b44fa45e40cab00bb6b1107313a92268
João Santiago <jcpsantiago@users.noreply.github.com>
2023-05-14 08:11:40 +0200

    add route for slash commands

--- src/jcpsantiago/arqivist/api/slack/router.clj
+++ src/jcpsantiago/arqivist/api/slack/router.clj
@@ -14,11 +14,18 @@
     {:post
      {:summary "Target for Message Shortcut interactions"
       :description "This endpoint receives all interactions initiated by clicking the Message Shortcut button. It also receives all follow-up interactions with the use via modals."
-
-      ;; TODO: learn how to set this up correctly :s
-      ;; :parameters {:form :jcpsantiago.arqivist.api.slack.spec/shortcut-body}
+      :parameters {:body :jcpsantiago.arqivist.api.slack.spec/shortcut-body}
       :responses {200 {:body string?}}
-      :handler handler/slack-shortcut}}]
+      :handler handler/message-shortcut}}]
+
+   ;; TODO: Add example from https://api.slack.com/interactivity/slash-commands#app_command_handling
+   ["/slash"
+    {:post
+     {:summary "Target for Slash Command interactions"
+      :description "This endpoint receives all interactions initiated by typing the `/arqive` slash command."
+      :parameters {:form :jcpsantiago.arqivist.api.slack.spec/slash-form-params}
+      :responses {200 {:body string?}}
+      :handler handler/slash-command}}]
 
    ["/redirect"
     {:get
