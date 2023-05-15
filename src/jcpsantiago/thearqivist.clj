feb42b9f62ef906d836f917bc2e635c9ee2f1ffe
João Santiago <jcpsantiago@users.noreply.github.com>
2023-05-15 11:02:55 +0200

    add -main fn using system

--- src/jcpsantiago/thearqivist.clj
+++ src/jcpsantiago/thearqivist.clj
@@ -1,21 +1,16 @@
-;; ---------------------------------------------------------
-;; jcpsantiago.thearqivist
-;; ---------------------------------------------------------
-
-
 (ns jcpsantiago.thearqivist
-  "the start of something great"
+  "The start of something great!"
   (:gen-class)
   (:require
+   [donut.system :as donut]
+   [jcpsantiago.arqivist.system :refer [system]]
    [com.brunobonacci.mulog :as mulog]))
-
 
 ;; ---------------------------------------------------------
 ;; Start Mulog publisher - only once
+
 (defonce mulog-publisher
   (mulog/start-publisher! {:type :console :pretty? true}))
-;; ---------------------------------------------------------
-
 
 ;; ---------------------------------------------------------
 ;; Application
@@ -23,35 +18,25 @@
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn greet
   "Greeting message via Clojure CLI clojure.exec"
-  ([] (greet {:team-name "secret engineering"}))
+  ([] (greet {:team-name "Bursting burrito"}))
   ([{:keys [team-name]}]
-   (str "thearqivist application developed by the " team-name " team")))
-
+   (str "The Arqivist is brought to you by " team-name)))
 
 (defn -main
-  "Entry point into the application via clojure.main -M"
+  "The Arqivist is managed by donut system.
+  The shutdown hook gracefully stops the service on receipt of a SIGTERM from the infrastructure,
+  giving the application 30 seconds before forced termination."
   [& args]
   (let [team (first args)]
     (mulog/set-global-context!
-     {:app-name "jcpsantiago-thearqivist" :version  "{{version}}"})
+     ;; TODO: get the version from a file or config, issue #23
+     {:app-name "The Arqivist" :version  "{{version}}"})
     (mulog/log ::application-starup :arguments args)
     (if team
       (greet team)
-      (greet))))
+      (greet)))
 
-;; ---------------------------------------------------------
-
-
-;; ---------------------------------------------------------
-;; Rick Comment
-#_{:clj-kondo/ignore [:redefined-var]}
-(comment
-
-  (-main)
-  (-main {:team-name "Clojure Engineering"})
-
-  ;; Stop mulog publisher
-  (mulog-publisher)
-
-  #_()) ; End of rich comment block
-;; ---------------------------------------------------------
+  (let [running-system (donut/signal system ::donut/start)]
+    (.addShutdownHook
+     (Runtime/getRuntime)
+     (Thread. ^Runnable #(donut/signal running-system ::donut/stop)))))
