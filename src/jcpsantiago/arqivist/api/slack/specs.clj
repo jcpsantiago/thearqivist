0bfd884920d008ff76a5f834cc6f5ade56e86878
João Santiago <jcpsantiago@users.noreply.github.com>
2023-06-01 11:21:47 +0100

    add non-blank-string?, remove unused specs

--- src/jcpsantiago/arqivist/api/slack/specs.clj
+++ src/jcpsantiago/arqivist/api/slack/specs.clj
@@ -3,15 +3,32 @@
   Specs for Slack data, including incoming requests,
   db representations and internal maps.
   "
-  (:require [clojure.spec.alpha :as spec]))
+  (:require
+   [clojure.spec.alpha :as spec]
+   [clojure.string :refer [blank?]]))
 
-(spec/def ::access_token string?)
-(spec/def ::scope string?)
-(spec/def ::bot_user_id string?)
+;; Extra predicates
+(defn non-blank-string?
+  "
+  Predicate for non blank strings.
+  "
+  [x]
+  (and (string? x) (not (blank? x))))
+
+(spec/def ::access_token non-blank-string?)
+(spec/def ::scope non-blank-string?)
+(spec/def ::bot_user_id non-blank-string?)
 (spec/def ::app_id string?)
 (spec/def ::id string?)
 (spec/def ::name string?)
 (spec/def ::token_type #{"bot" "user"})
+
+;; Error response
+(spec/def ::ok boolean?)
+(spec/def ::error string?)
+(spec/def ::error-response
+  (spec/keys
+   :req-un [::ok ::error]))
 
 (spec/def ::team
   (spec/keys
@@ -23,18 +40,23 @@
    :req-un [::id]
    :opt-un [::scope ::access_token ::token_type]))
 
-;; OAuth redirect
-(spec/def ::code string?)
-(spec/def ::state (spec/nilable string?))
+;; OAuth redirect -----------------------------------------------
+(spec/def ::code non-blank-string?)
+(spec/def ::state non-blank-string?)
 
+;; Initial request received from Slack once user allows the requested scopes
+;; code is exchanged for a request token (see spec below)
+;; official docs in https://api.slack.com/authentication/oauth-v2
 (spec/def ::oauth-redirect
   (spec/keys
-    :req-un [::code ::state]))
+   :req-un [::code ::state]))
 
 ;; Access token request
-(spec/def ::access-token-request
-  (spec/keys
-   :req-un [::access_token ::scope ::bot_user_id ::app_id ::team ::authed_user]))
+(spec/def ::oauth-access
+  (spec/or
+   :good-response (spec/keys
+                   :req-un [::access_token ::scope ::bot_user_id ::app_id ::team ::authed_user])
+   :error-response ::error-response))
 
 ;; Slash command request
 ;; Slash commands are sent via POST requests with Content-type application/x-www-form-urlencoded.
@@ -76,81 +98,8 @@
   (spec/keys
    :req-un [::type ::team ::user ::view]))
 
-
 ;; Internal representations ------------------------------------------------
 (spec/def ::team-attributes
   (spec/keys
    :req-un [::id ::uuid ::app_id ::external_team_id ::team_name ::registering_user
             ::scopes ::access_token ::bot_user_id ::created_at]))
-
-;; API responses -----------------------------------------------------------
-(spec/def ::ok boolean?)
-(spec/def ::error string?)
-(spec/def ::warning string?)
-(spec/def ::api-error
-  (spec/keys
-   :req-un [::ok ::error]))
-
-
-(spec/def ::next_cursor string?)
-(spec/def ::response_metadata
-  (spec/keys
-   :req-un [::next_cursor]))
-
-
-(spec/def ::channel
-  (spec/keys
-   :req-un [::id ::name ::name_normalized ::is_channel ::created ::creator
-            ::is_archived ::is_general ::is_shared ::parent_conversation
-            ::is_ext_shared ::is_pending_ext_shared ::is_org_shared ::is_member
-            ::is_private ::is_mpim ::last_read ::topic ::purpose ::previous_names]
-   :opt-un [::warning]))
-
-(spec/def ::convo-info
-  (spec/or
-   ::api-error
-   (spec/keys
-    :req-un [::ok ::channel])))
-
-(spec/def ::convo-members
-  (spec/or
-   ::api-error
-   (spec/keys
-    :req-un [::ok ::members ::response_metadata])))
-
-(spec/def ::profile
-  (spec/keys
-   :req-un [::title ::phone ::skype ::real_name ::real_name_normalized
-            ::display_name ::display_name_normalized ::status_text
-            ::status_emoji ::status_expiration ::email ::first_name
-            ::last_name]))
-
-(spec/def ::user-profile
-  (spec/or
-   ::api-error
-   (spec/keys
-    :req-un [::ok ::profile])))
-
-(spec/def ::messages
-  (spec/keys
-   :req-un [::type ::user ::text ::ts]
-   :opt-un [::attachments ::subtype ::hidden ::is_starred
-            ::pinned_to ::reactions]))
-
-(spec/def ::convo-history
-  (spec/or
-   ::api-error
-   (spec/keys
-    :req-un [::ok ::messages ::has_more ::pin_count ::response_metadata])))
-
-(spec/def ::convo-join
-  (spec/or
-   ::api-error
-   (spec/keys
-    :req-un [::ok ::channel])))
-
-(spec/def ::convo-replies
-  (spec/or
-   ::api-error
-   (spec/keys
-    :req-un [::ok ::messages])))
