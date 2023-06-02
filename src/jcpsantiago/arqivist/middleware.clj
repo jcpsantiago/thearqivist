53a05c5b3c4e9f1a3927044185db380356eaf37a
João Santiago <jcpsantiago@users.noreply.github.com>
2023-06-02 15:08:47 +0100

    id var is never used

--- src/jcpsantiago/arqivist/middleware.clj
+++ src/jcpsantiago/arqivist/middleware.clj
@@ -36,7 +36,7 @@
 (defn verify-atlassian-lifecycle
   "Middleware to verify the JWT token present in
   Atlassian lifecycle installed/uninstalled events."
-  [handler id]
+  [handler _]
   (fn [request]
     (let [jwt-token (-> (or (get-in request [:headers "authorization"]) "")
                         (string/replace #"JWT\s" ""))]
@@ -56,4 +56,3 @@
                      :local-time (java.time.LocalDateTime/now))
           ;; Atlassian only fails on 500, all other statuses will be seen as "OK"
           {:status 500 :body ""})))))
-
