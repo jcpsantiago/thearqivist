3edcf0bc319498ad5932407979e69c190a4a7adc
João Santiago <me@jcpsantiago.xyz>
2023-08-16 11:38:15 +0200

    add confirm-save-modal

--- src/jcpsantiago/arqivist/api/slack/ui_blocks.clj
+++ src/jcpsantiago/arqivist/api/slack/ui_blocks.clj
@@ -21,7 +21,11 @@
      :elements
      [{:type "mrkdwn",
        :text
-       "👀 View all channels being archived with `/arqive jobs`\n✅ Save a channel once with `/arqive once`\n📓 Continuosly save the current channel with `/arqive daily [or weekly or monthly]`\n❌ Stop saving the current channel with `/arqive stop`"}]}
+       "
+       ✅ See options for saving a channel with `/arqive`
+       👀 View all channels being archived with `/arqive jobs`\n
+       ❌ Stop saving the current channel with `/arqive stop`
+       "}]}
     {:type "section", :text {:type "mrkdwn", :text " "}}
     {:type "section",
      :text
@@ -33,3 +37,53 @@
      :elements
      [{:type "mrkdwn",
        :text "❓ Get help at any time with `/arqive help`"}]}]})
+
+(defn confirm-save-modal
+  "
+ Modal asking the user to confirm saving a channel once.
+ "
+  [request]
+  (let [channel_name (get-in request [:parameters :form :channel_name])]
+    {:type "modal"
+     :title {:type "plain_text" :text "The Arqivist" :emoji true}
+     :submit {:type "plain_text" :text "Create archive" :emoji true}
+     :close {:type "plain_text" :text "Cancel" :emoji true}
+     :blocks
+     [{:type "section"
+       :text
+       {:type "mrkdwn"
+        :text (str "Looks like you want to create an archive from "
+                   "*#" channel_name "*")}}
+      {:type "divider"}
+      {:type "input"
+       :element
+       {:type "radio_buttons"
+        :options
+        [{:text {:type "plain_text" :text "Once" :emoji true}
+          :value "once"}
+         {:text {:type "plain_text" :text "Daily" :emoji true}
+          :value "daily"}
+         {:text {:type "plain_text" :text "Weekly" :emoji true}
+          :value "weekly"}]
+        :action_id "radio_buttons-action"}
+       :block_id "archive_frequency_selector"
+       :label
+       {:type "plain_text"
+        :text "How often do you want to archive it?"
+        :emoji true}}
+      {:type "context"
+       :elements
+       [{:type "mrkdwn"
+         :text
+         "For *daily* and *weekly*: the archive is created _now_, then updated at 12am with the frequency you selected."}]}
+      {:type "section"
+       :text
+       {:type "mrkdwn"
+        :text
+        (str "Once the archive is created I'll inform " "*#" channel_name "* and share a link to the archive :handshake:")}}
+      {:type "divider"}
+      {:type "context"
+       :elements
+       [{:type "mrkdwn"
+         :text "For more info use the `/arqive help` command."}]}]}))
+
